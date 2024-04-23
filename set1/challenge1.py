e61649a4dff43ed9cc01411ff1dae2d6c2ae597a
Etienne Donneger <0237h@pm.me>
2024-04-23 00:31:55 -0400

    Validate Challenge 1

---
+++ set1/challenge1.py
@@ -0,0 +1,14 @@
+from base64 import b64encode
+
+
+def hex_to_base64(input_hex: bytes) -> bytes:
+    return b64encode(input_hex)
+
+
+def test():
+    assert (
+        hex_to_base64(
+            bytes.fromhex(
+                "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+        ) == b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    )

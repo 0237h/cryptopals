a6f3fbb9bd78ad1d9a41633ee57bdd0e31784c6a
Etienne Donneger <0237h@pm.me>
2024-04-27 17:50:36 -0400

    Validate Challenge 8

---
+++ set1/challenge8.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+
+PATTERN_LENGTH = 16
+
+
+def test():
+    line_count = 1
+    possible_ecb = (line_count, "")
+    for line in open("./set1/challenge_8.txt"):
+        line = line.strip()
+        patterns = [
+            (line_count, pattern, count) for pattern, count in Counter(
+                [line[i:i+PATTERN_LENGTH]
+                 for i in range(0, len(line), PATTERN_LENGTH)]
+            ).items() if count > 1
+        ]
+
+        if patterns:
+            print(patterns)
+            possible_ecb = (line_count, line)
+
+        line_count += 1
+
+    # Found after running program successfully first
+    assert (possible_ecb[0] == 133)
+    assert (possible_ecb[1] == (
+        "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f"
+        "4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a"
+        "6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
+    ))
+
+
+if __name__ == '__main__':
+    test()

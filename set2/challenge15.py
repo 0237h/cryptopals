21b9c0160957c0de269f35270bbb36f5cec57f4a
Etienne Donneger <0237h@pm.me>
2024-05-02 08:31:23 -0400

    Validate Challenge 15

---
+++ set2/challenge15.py
@@ -0,0 +1,29 @@
+def validate_pkcs7(plaintext: bytes) -> str:
+    padding_byte = plaintext[-1]
+
+    if not plaintext[-padding_byte:].count(padding_byte) == padding_byte:
+        raise ValueError("Not a valid PKCS#7 padding")
+
+    return plaintext[:-padding_byte].decode()
+
+
+def test():
+    plaintext = b"ICE ICE BABY\x04\x04\x04\x04"
+    assert (validate_pkcs7(plaintext) == "ICE ICE BABY")
+
+    print(f"Plaintext:\n{plaintext}")
+    print(f"Plaintext (stripped):\n{validate_pkcs7(plaintext)}")
+
+    try:
+        validate_pkcs7(b"ICE ICE BABY\x05\x05\x05\x05")
+    except ValueError as e:
+        assert (str(e) == "Not a valid PKCS#7 padding")
+
+    try:
+        validate_pkcs7(b"ICE ICE BABY\x01\x02\x03\x04")
+    except ValueError as e:
+        assert (str(e) == "Not a valid PKCS#7 padding")
+
+
+if __name__ == "__main__":
+    test()

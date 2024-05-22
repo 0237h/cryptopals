1dc3da91c009f7f8a7918386107941eff5adfdc1
Etienne Donneger <0237h@pm.me>
2024-05-22 08:14:06 -0400

    Add endianness parameter for AES CTR decryption

--- set3/challenge18.py
+++ set3/challenge18.py
@@ -2,10 +2,11 @@ from base64 import b64decode
 
 import sys
 from os import path
+from typing import Literal
 
 sys.path.insert(1, path.abspath("set1"))  # Python hack to resolve set1/ challenges imports
 
-from challenge7 import _keyExpansion, _aes128, _invAes128  # noqa # type: ignore
+from challenge7 import _keyExpansion, _aes128  # noqa # type: ignore
 from challenge2 import xor  # noqa # type: ignore
 
 sys.path.insert(1, path.abspath("set2"))
@@ -41,7 +42,8 @@ def decrypt_aes128_ctr(
     ciphertext: bytes,
     key: bytes,
     nonce: bytes | None = None,
-    strip_padding: bool = True
+    strip_padding: bool = True,
+    counter_endianness: Literal["little", "big"] = "big"
 ) -> bytearray:
     assert len(key) == 16, "Key must be 128 bits"
 
@@ -56,7 +58,7 @@ def decrypt_aes128_ctr(
     for i in range(0, len(ciphertext), 16):
         output_bytes.extend(
             xor(
-                _aes128(nonce + (i // 16).to_bytes(8, "little"), keys),
+                _aes128(nonce + (i // 16).to_bytes(8, counter_endianness), keys),
                 ciphertext[i:i+16]
             )
         )
@@ -71,7 +73,7 @@ def test():
     ciphertext = b64decode("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
     key = b"YELLOW SUBMARINE"
 
-    assert (decrypt_aes128_ctr(ciphertext, key, strip_padding=False) ==
+    assert (decrypt_aes128_ctr(ciphertext, key, strip_padding=False, counter_endianness="little") ==
             b"Yo, VIP Let\'s kick it Ice, Ice, baby Ice, Ice, baby ")
 
     plaintext = b"Hello World!"
